7c26fa14e64cba51e41aa31c379c2b665c89d6fc
liz9999 <lizkong99@gmail.com>
2023-07-02 22:03:15 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import react, { useState } from 'react'
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import VideoLibrary from './components/VideoLibrary'
 
 function App() {
-  
-  const [ Group, setGroup ] = useState(null);
 
   return (
     <div className="App">
