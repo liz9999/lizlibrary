c3b1de264fdb6aa7618a7946741d8e905b5cda84
liz9999 <lizkong99@gmail.com>
2023-07-03 00:00:11 -0400

    update

--- src/components/VideoLibrary.js
+++ src/components/VideoLibrary.js
@@ -1,10 +1,11 @@
 import React from "react";
 import VideoItem from "../components/VideoItem"
+import { VideoList } from '../helpers/VideoList'
 
 const VideoLibrary = ({ListOfVideos}) => {
     return (
       <div className="video">
-        <h1 className="videoTitle">Latest LIVEs</h1>
+        <h1 className="videoTitle">{VideoList.length === ListOfVideos.length ? "Latest LIVEs" : ListOfVideos.length + " LIVEs"}</h1>
         <div className="videoList">
           {ListOfVideos.map((videoItem, key) => {
             return (
